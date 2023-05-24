a0dac7a6fa78193b0563bd5fff7f8bdb7e3d56d2
Ryan Ng <ryan.ng.dev@tinynamoo.com>
2023-05-25 03:18:52 +0800

    Add palette and themes styling
    Create palette and themes files for cleaner styling

---
+++ app/styles/Palette.ts
@@ -0,0 +1,11 @@
+/**
+ * App palette
+ *
+ * @format
+ */
+
+export const palette = {
+  purple: '#efe3fb',
+  darkPurple: '#5c70ea',
+  darkBlue: '#100d40',
+};
---
+++ app/styles/themes.ts
@@ -0,0 +1,21 @@
+/**
+ * Themes of the application
+ *
+ * @format
+ */
+
+import {palette} from './Palette';
+
+export const themes = {
+  color: {
+    appBackground: palette.purple,
+    buttonBackgroundPrimary: palette.darkBlue,
+    primary: palette.darkBlue,
+    appNameSecondary: palette.darkPurple,
+  },
+  font: {
+    header: 'Roxborough-CF-Bold',
+    subtitle: 'Alata',
+    body: 'Poppins-Regular',
+  },
+};
