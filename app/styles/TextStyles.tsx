421757c9aa22755d9648f35980c24968435b2d1b
Ryan Ng <ryan.ng.dev@tinynamoo.com>
2023-05-25 01:51:47 +0800

    Add global text styles for headers and subtitles
    - Add fonts used for header and subtitles.
    - Add global stylesheet for header and subtitles.

---
+++ app/styles/TextStyles.tsx
@@ -0,0 +1,16 @@
+/**
+ * Text Styles for the app
+ *
+ * @format
+ */
+
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  headerText: {
+    fontFamily: 'Roxborough-CF-Bold',
+  },
+  subtitleText: {
+    fontFamily: 'Alata',
+  },
+});
---
+++ react-native.config.js
@@ -0,0 +1,9 @@
+/**
+ * React Native configuration file
+ *
+ * @format
+ */
+
+module.exports = {
+  assets: ['./app/assets/fonts/'],
+};
