2a0d91eb18cf5ee0418f0e35643efb2557a39b96
Ryan Ng <ryan.ng.dev@tinynamoo.com>
2023-05-26 15:42:50 +0800

    Add new palette color and themes
    - Add new lightgray color to palette
    - Add screen size flex values to theme
    - Add new font for bolded body text
    - Add new text style for bolded body text

--- app/styles/themes.ts
+++ app/styles/themes.ts
@@ -14,11 +14,17 @@ export const themes = {
     textDarkBackgound: palette.white,
     textLightBackground: palette.darkBlue,
     appNameSecondary: palette.darkPurple,
+    textInputColor: palette.lightGray,
   },
   font: {
     header: 'Roxborough-CF-Bold',
     subtitle: 'Alata',
     body: 'Poppins-Regular',
+    bodyBold: 'Poppins-SemiBold',
     button: 'Poppins-SemiBold',
+  },
+  sizes: {
+    horizontalScreenSize: 8,
+    verticalScreenSize: 15,
   },
 };
