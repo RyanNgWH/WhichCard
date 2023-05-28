56be65eb0fd05a053a695ca924d5e3ba2ef38b4c
Ryan Ng <ryan.ng.dev@tinynamoo.com>
2023-05-28 21:45:47 +0800

    Add navigation routes
    - Add navigation routes for the following
      - Landing page
      - Sign up page
      - Login page
    - Set inital route to landing page

--- App.tsx
+++ App.tsx
@@ -1,7 +1,21 @@
-import LandingPage from './app/screens/LandingScreen';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import LandingScreen from './app/screens/LandingScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import LoginScreen from './app/screens/LoginScreen';
+import SignUpScreen from './app/screens/SignUpScreen';
+
+const Stack = createNativeStackNavigator();
 
 function App() {
-  return <LandingPage />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Landing">
+        <Stack.Screen name="Landing" component={LandingScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
