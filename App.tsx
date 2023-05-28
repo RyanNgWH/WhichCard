a725d12db05adef6fa3610a526263de0d5ec6d39
Chee Jing Jie <42177782+Avosthe@users.noreply.github.com>
2023-05-29 01:33:02 +0800

    Implement sign up and login feature without navigation. (#7)
    # Description
    Implement sign up and login features.
    
    # Changelog
    - Add react-native-config dependency to introduce environment variables.
    - Create .env file in root directory of project.
    - Add back-end api server url to .env file to enable accessibility as an environment variable.
    - Add axios dependency for http request capability.
    - Establish link between front-end and back-end.
    - Added error messages to sign up and login form.

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
 import LandingPage from './app/screens/LandingScreen';
+import SignUpPage from './app/screens/SignUpScreen';
+import LoginPage from './app/screens/LoginScreen';
 
 function App() {
   return <LandingPage />;
