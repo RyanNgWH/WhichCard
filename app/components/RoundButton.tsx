c2811a7bd4a571bcb0aa4370b0482eabeccad5b7
Ryan Ng <ryan.ng.dev@tinynamoo.com>
2023-06-01 15:37:42 +0800

    Fix button press overlay too big bug
    - Add handlers for button press in and out
    - Modify button styles to match project format
    - Change button text to use button text style
    - Remove unnecessary view wrapping button component

--- app/components/RoundButton.tsx
+++ app/components/RoundButton.tsx
@@ -7,6 +7,7 @@
 import {Text, ViewStyle, Pressable, View, StyleSheet} from 'react-native';
 import TextStyles from '../styles/TextStyles';
 import {Themes} from '../styles/Themes';
+import {useState} from 'react';
 
 // Props for round button
 type RoundButtonProps = {
@@ -18,51 +19,69 @@ type RoundButtonProps = {
 
 // Round button component
 function RoundButton(props: RoundButtonProps) {
+  // State for pressed
+  const [isPressed, setIsPressed] = useState(false);
+
+  // Handle press in round button
+  const handlePressIn = () => {
+    setIsPressed(true);
+  };
+
+  // Handle press out in round button
+  const handlePressOut = () => {
+    setIsPressed(false);
+  };
+
   return (
-    <View style={props.style}>
-      <Pressable
-        onPress={props.onPress}
-        style={({pressed}) => pressed && styles.pressed}>
-        <View
-          style={[styles.button, props.mode === 'outlined' && styles.outlined]}>
-          <Text
-            style={[
-              TextStyles({theme: 'dark'}).buttonText,
-              styles.buttonText,
-              props.mode === 'outlined' &&
-                TextStyles({theme: 'light'}).bodyText,
-            ]}>
-            {props.children}
-          </Text>
-        </View>
-      </Pressable>
-    </View>
+    <Pressable
+      onPress={props.onPress}
+      onPressIn={handlePressIn}
+      onPressOut={handlePressOut}
+      style={[isPressed && roundButtonStyles().pressed, props.style]}>
+      <View
+        style={[
+          roundButtonStyles().button,
+          props.mode === 'outlined' && roundButtonStyles().outlined,
+        ]}>
+        <Text
+          style={[
+            TextStyles({theme: 'dark'}).buttonText,
+            roundButtonStyles().buttonText,
+            props.mode === 'outlined' &&
+              TextStyles({theme: 'light'}).buttonText,
+          ]}>
+          {props.children}
+        </Text>
+      </View>
+    </Pressable>
   );
 }
 
-const styles = StyleSheet.create({
-  button: {
-    borderRadius: 25,
-    padding: 12,
-    backgroundColor: Themes.colors.buttonBackgroundPrimary,
-    borderWidth: 1,
-  },
-  outlined: {
-    backgroundColor: 'transparent',
-    borderColor: Themes.colors.buttonBackgroundPrimary,
-  },
-  buttonText: {
-    textAlign: 'center',
-    fontSize: 16,
-  },
-  outlinedText: {
-    color: Themes.colors.textLightBackground,
-  },
-  pressed: {
-    opacity: 0.75,
-    borderRadius: 25,
-    backgroundColor: Themes.colors.buttonBackgroundSecondary,
-  },
-});
+// Styles for round button
+const roundButtonStyles = () =>
+  StyleSheet.create({
+    button: {
+      borderRadius: 25,
+      padding: 12,
+      backgroundColor: Themes.colors.buttonBackgroundPrimary,
+      borderWidth: 1,
+    },
+    outlined: {
+      backgroundColor: 'transparent',
+      borderColor: Themes.colors.buttonBackgroundPrimary,
+    },
+    buttonText: {
+      textAlign: 'center',
+      fontSize: 16,
+    },
+    outlinedText: {
+      color: Themes.colors.textLightBackground,
+    },
+    pressed: {
+      opacity: 0.75,
+      borderRadius: 25,
+      backgroundColor: Themes.colors.buttonBackgroundSecondary,
+    },
+  });
 
 export default RoundButton;
