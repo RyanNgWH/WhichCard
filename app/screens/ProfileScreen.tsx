8569c0c8370df08509ae6bd9725ee8b9323280e6
Ryan Ng <ryan.ng.dev@tinynamoo.com>
2023-06-20 18:38:49 +0800

    Add placeholder screens
    - Add placeholder screens for the following screens
      - Profile
      - Transactions
      - Report

---
+++ app/screens/ProfileScreen.tsx
@@ -0,0 +1,17 @@
+/**
+ * Profile Screen
+ *
+ * @format
+ */
+
+import {Text, View} from 'react-native';
+
+function ProfileScreen() {
+  return (
+    <View>
+      <Text>Profile Screen</Text>
+    </View>
+  );
+}
+
+export default ProfileScreen;
---
+++ app/screens/TransactionsScreen.tsx
@@ -0,0 +1,17 @@
+/**
+ * Transactions Screen
+ *
+ * @format
+ */
+
+import {Text, View} from 'react-native';
+
+function TransactionsScreen() {
+  return (
+    <View>
+      <Text>Transactions Screen</Text>
+    </View>
+  );
+}
+
+export default TransactionsScreen;
